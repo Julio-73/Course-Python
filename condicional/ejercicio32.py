529a07a2f2f2c9dc5642eb2c6754f6435f212feb
julio-cesar <112985882+Julio-73@users.noreply.github.com>
2023-06-30 22:43:48 -0500

    -- new commit --

---
+++ condicional/ejercicio32.py
@@ -0,0 +1,21 @@
+# Else if
+
+number= 400
+number1= 500
+
+print('Escribe un numero')
+x=input()
+
+#condicional
+if number< number1 :
+    print('es correcto ')
+else :
+    print('No es correcto')
+
+  
+
+
+
+
+
+
---
+++ condicional/ejercicio33.py
@@ -0,0 +1,15 @@
+# elif
+
+a=700
+b=700
+
+if a>b :
+    print('esto es correcto')
+elif a==b :
+    print('esto es igual')
+else:
+    print('ninguno es correcto')
+
+
+
+
