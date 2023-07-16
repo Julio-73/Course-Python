4f0f142a283c035c6ffc3e51e914ed38c971afa0
julio-cesar <112985882+Julio-73@users.noreply.github.com>
2023-07-15 20:20:54 -0500

    -- new commit --

--- set/ejercicio75.py
+++ set/ejercicio75.py
@@ -1,2 +1,5 @@
 # obtener valores de dicionario
 
+nombres={'nombre':'oscar','sitio':'dev.com','canal':'devs','edad':39}
+print(nombres['sitio'])
+print(nombres.get('sitio')) # get puede ver el indice especifico !
---
+++ set/ejercicio76.py
@@ -0,0 +1,13 @@
+# borrar datos de set
+
+nombres= {'oscar','josue','pedro'}
+print(nombres)
+nombres.remove('oscar')
+print(nombres)
+nombres.add('lopez') # add te agrega un dato
+print(nombres)
+nombres.pop() # pop te borra el primer dato
+print(nombres)
+nombres.clear() # clear elimina todos los datos
+print(nombres)
+
